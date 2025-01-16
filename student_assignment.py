e08f2019717265ea0053da1d5c77ceb590c6e035
YWChen1169 <yw_chen@asus.com>
2025-01-16 17:17:45 +0800

    hw01 hw02 hw03 hw04

--- student_assignment.py
+++ student_assignment.py
@@ -1,6 +1,6 @@
 import json
 import traceback
-
+import requests
 from model_configurations import get_model_configuration
 
 from langchain_openai import AzureChatOpenAI
@@ -92,15 +92,13 @@ def generate_hw02(question):
             response = requests.get(url)
             response.raise_for_status()  # Raises an HTTPError for bad responses
             data = response.json()  # Parse JSON response into a dictionary
-
-            holidays = data.get('response', {}).get('holidays', [])
             output_data = {
                 "Result": [
                 {
                     "date": holiday['date']['iso'],
                     "name": holiday['name']
                 }
-                for holiday in holidays['response']['holidays']
+                for holiday in data['response']['holidays']
             ]
             }
 
@@ -114,7 +112,7 @@ def generate_hw02(question):
         except Exception as err:
             print(f'An error occurred: {err}')
 
-    return {"Result": []}  # Return an empty result in case of error
+        return {"Result": []}  # Return an empty result in case of error
     tools = [check_calendarific]
 
     llm = AzureChatOpenAI(
@@ -197,7 +195,178 @@ def generate_hw02(question):
 
     
 def generate_hw03(question2, question3):
-    pass
+
+    @tool
+    def check_calendarific(country : str, year : str, month : str) -> dict:
+        """  use the calendarific api to check all the holiday with the str parameter country, year, month. 
+            year and month is string
+        """
+        api_key = 'WjJ5eItS7tMLp9x0c1SS1x7HxaGSTq9t'
+        url = f'https://calendarific.com/api/v2/holidays?&api_key={api_key}&country={country}&year={year}&month={month}'
+
+        try:
+            response = requests.get(url)
+            response.raise_for_status()  # Raises an HTTPError for bad responses
+            data = response.json()  # Parse JSON response into a dictionary
+            output_data = {
+                "Result": [
+                {
+                    "date": holiday['date']['iso'],
+                    "name": holiday['name']
+                }
+                for holiday in data['response']['holidays']
+            ]
+            }
+
+            return output_data
+        except requests.exceptions.HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}')
+        except requests.exceptions.RequestException as req_err:
+            print(f'Request error occurred: {req_err}')
+        except json.JSONDecodeError as json_err:
+            print(f'Error decoding JSON: {json_err}')
+        except Exception as err:
+            print(f'An error occurred: {err}')
+
+        return {"Result": []}  # Return an empty result in case of error
+
+    @tool
+    def organize_the_answer(add : bool, reason : str ) -> dict:
+        """
+        after answer the question : Based on the previous holiday lists, the holidays on the list for that mont?. and use this tool to organize the answer.
+        add : If the answer is  included in the list of holidays then return true ,  otherwise, it returns false.
+        reason : Describe why you do or do not want to add a new holiday, specify whether the holiday already exists in the list, and the contents of the current list.
+        """
+        output_data = {
+            "Result":
+            {
+                "add": add,
+                "reason": reason
+            }
+        }
+
+        return output_data
+
+
+    tools = [check_calendarific,organize_the_answer]
+
+    llm = AzureChatOpenAI(
+            model=gpt_config['model_name'],
+            deployment_name=gpt_config['deployment_name'],
+            openai_api_key=gpt_config['api_key'],
+            openai_api_version=gpt_config['api_version'],
+            azure_endpoint=gpt_config['api_base'],
+            temperature=gpt_config['temperature'],
+            # model_kwargs={ "response_format": { "type": "json_object" } }
+    )
+
+
+    examples = [
+        {
+        "input":"2024年台灣10月紀念日有哪些?",
+        "output":
+        """
+        {
+                "Result": [
+                    {
+                        "date": "2024-10-10",
+                        "name": "National Day"
+                    },
+                    {
+                        "date": "2024-10-09",
+                        "name": "Double Ninth Day"
+                    },
+                    {
+                        "date": "2024-10-21",
+                        "name": "Overseas Chinese Day"
+                    },
+                    {
+                        "date": "2024-10-25",
+                        "name": "Taiwan's Retrocession Day"
+                    },
+                    {
+                        "date": "2024-10-31",
+                        "name": "Halloween"
+                    }
+                ]
+            }
+            """
+        },
+    ]
+
+    example_prompt = ChatPromptTemplate.from_messages(
+            [
+            ("human", "{input}"),
+            ("ai", "{output}"),
+            ]
+        )
+    few_shot_promt = FewShotChatMessagePromptTemplate(
+            example_prompt=example_prompt,
+            examples=examples,
+        )
+
+    # Define the templates
+    system_prompt = SystemMessagePromptTemplate(
+        prompt=PromptTemplate(input_variables=[], 
+            template="You are a helpful assistant,  provide the response in JSON format. "
+            "the output data sturctured was like the examples, provide the json data structure")
+    )
+
+    # Placeholder for chat history
+    chat_history_placeholder = MessagesPlaceholder(variable_name='chat_history', optional=True)
+
+    # Human message template
+    human_message_template = HumanMessagePromptTemplate(
+        prompt=PromptTemplate(input_variables=['input'], template="{input}")
+    )
+
+    # Placeholder for agent scratchpad
+    agent_scratchpad_placeholder = MessagesPlaceholder(variable_name='agent_scratchpad')
+
+    # Combine the parts into a full prompt
+    final_prompt = system_prompt + chat_history_placeholder + human_message_template + agent_scratchpad_placeholder
+
+    # print(final_prompt.messages)
+
+
+    agent = create_openai_functions_agent(llm = llm, tools = tools, prompt = final_prompt)
+    agent_executor = AgentExecutor(agent = agent, tools = tools, verbose = True)
+
+    #Adding in memory
+    store = {}
+    def get_session_history(session_id: str) -> BaseChatMessageHistory:
+        if session_id not in store:
+            store[session_id] = ChatMessageHistory()
+        return store[session_id]
+
+    agent_with_chat_history = RunnableWithMessageHistory(
+        agent_executor,
+        get_session_history,
+        input_messages_key="input",
+        history_messages_key="chat_history",
+    )
+
+    message2 = HumanMessage(
+            content=[
+                {"type": "text", "text": question2},
+            ]
+    )
+
+    message3 = HumanMessage(
+            content=[
+                {"type": "text", "text": question3},
+            ]
+    )
+
+
+    response = agent_with_chat_history.invoke({"input" : message2 }, config={"configurable": {"session_id" : "<foo>"}},)
+    # print(response)
+
+    response = agent_with_chat_history.invoke({"input" : message3}, config={"configurable": {"session_id" : "<foo>"}},)
+    # print(response)
+
+    return response["output"].strip('```json\n').strip('```')
+
 
 def generate_hw04(question):
 
